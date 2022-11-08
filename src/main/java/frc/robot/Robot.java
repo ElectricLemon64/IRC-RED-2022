e9bf8010ccdea70f20fe2ac674cbe88b6281f8ea
Joey Qi <gnuffyway1@gmail.com>
2022-11-07 16:31:00 -0800

    fix

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -6,36 +6,28 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import edu.wpi.first.wpilibj.motorcontrol.MotorController;
-import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
-
+import frc.robot.subsystems.Drivetrain;
+import frc.robot.commands.Drive;
 /**
  * This is a demo program showing the use of the DifferentialDrive class, specifically it contains
  * the code necessary to operate a robot with tank drive.
  */
 public class Robot extends TimedRobot {
-  private DifferentialDrive m_myRobot;
-  private Joystick m_leftStick;
-  private Joystick m_rightStick;
+  private Joystick m_leftStick = new Joystick(0);
+  private Joystick m_rightStick = new Joystick(1);
 
-  private final MotorController m_leftMotor = new PWMSparkMax(0);
-  private final MotorController m_rightMotor = new PWMSparkMax(1);
+  Drivetrain drivetrain = new Drivetrain();
+  Drive drive = new Drive(drivetrain,m_leftStick, m_rightStick);
 
   @Override
   public void robotInit() {
     // We need to invert one side of the drivetrain so that positive voltages
     // result in both sides moving forward. Depending on how your robot's
     // gearbox is constructed, you might have to invert the left side instead.
-    m_rightMotor.setInverted(true);
-
-    m_myRobot = new DifferentialDrive(m_leftMotor, m_rightMotor);
-    m_leftStick = new Joystick(0);
-    m_rightStick = new Joystick(1);
   }
 
   @Override
   public void teleopPeriodic() {
-    m_myRobot.tankDrive(m_leftStick.getY(), m_rightStick.getY());
+    drive.execute();
   }
 }
---
+++ src/main/java/frc/robot/commands/Drive.java
@@ -0,0 +1,35 @@
+package frc.robot.commands;
+
+import frc.robot.subsystems.Drivetrain;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+public class Drive extends CommandBase {
+    Drivetrain drivetrain;
+    Joystick leftJoystick;
+    Joystick rightJoystick;
+
+    public Drive(Drivetrain dt, Joystick leftJ, Joystick rightJ) {
+        drivetrain = dt;
+        leftJoystick = leftJ;
+        rightJoystick = rightJ;
+
+        super.addRequirements(dt);
+    }
+
+    public void execute() {
+        drivetrain.drive(leftJoystick.getY(), rightJoystick.getY());
+    }
+
+    public void initialize(){
+        drivetrain.stop();
+    }
+
+    public boolean isFinished() {
+        return false;
+    }
+
+    public void end(boolean stop) {
+        drivetrain.stop();
+    }
+}
